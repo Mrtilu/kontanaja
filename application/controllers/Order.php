9917fc0f017482ed25a82f5d03252440e9db4e0e
faizuddin wasis <mis.stf05@lnk.co.id>
2023-03-06 21:11:19 +0700

    fix

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -8,10 +8,10 @@ class Order extends CI_Controller
   {
     parent::__construct();
     $this->load->database();
-    $this->load->library(['ion_auth', 'form_validation']);
-    $this->load->helper(['url', 'language']);
-    $this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
-    $this->lang->load('auth');
+    // $this->load->library(['ion_auth', 'form_validation']);
+    // $this->load->helper(['url', 'language']);
+    // $this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
+    // $this->lang->load('auth');
     // if (!AVAIL) {
     //   if ($this->ion_auth->is_admin()) {
     //   } else {

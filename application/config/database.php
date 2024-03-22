da0be55ef979374a44ec4a9b427439b7135dd368
Mrtilu <159797418+Mrtilu@users.noreply.github.com>
2024-03-22 22:43:53 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -7,11 +7,11 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost', // sg-dc-a1.oriontekno.com
-	'username' => 'prod_kontanaja_db',
-	'password' => 'etKY5Ds5mUk0g03E',
-	'database' => 'prod_kontanaja_db',
-	'dbdriver' => 'mysqli',
+	'hostname' => 'user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com', // sg-dc-a1.oriontekno.com
+	'username' => 'kontanaja-main-db-0462cc8fdfe675dfe',
+	'password' => 'ybMBwxYBWQkmJPJsbDj22PFDcdA8N1',
+	'database' => 'kontanaja-main-db-0462cc8fdfe675dfe',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
